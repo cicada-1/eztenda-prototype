abea73a85879a1dd62e67fcc3fe2465ee812c822
Peter Wallace <peterwallace@hotmail.co.uk>
2021-03-20 12:04:48 +0000

    Added reference to brands from bids

---
+++ db/migrate/20210320120025_add_reference_to_bids.rb
@@ -0,0 +1,5 @@
+class AddReferenceToBids < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :bids, :brand
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_114210) do
+ActiveRecord::Schema.define(version: 2021_03_20_120025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_03_20_114210) do
     t.boolean "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "brand_id"
+    t.index ["brand_id"], name: "index_bids_on_brand_id"
   end
 
   create_table "brands", force: :cascade do |t|
