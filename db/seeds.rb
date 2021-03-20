f7c0d0231c7d9cb1085ab54bff44ed7ef1b37524
gayatriarora <gayatri7arora@gmail.com>
2021-03-20 19:48:31 +0000

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,8 +27,8 @@ user12 = User.create!(email: "user12@mail.com", password: "123456")
 puts "creating 6 venues"
 #Venues
 venue1 = Venue.create!(venue_name: "Bar Stock Exchange",
-                       email: "barstockex@mail.com", 
-                       password: "123456",
+                    #    email: "barstockex@mail.com", 
+                    #    password: "123456",
                        address: "Madison Square, Left Avenue WO 0YT", 
                        phone_number: "+44986453625", 
                        description: "Lets play drink-stocks!",
@@ -36,8 +36,8 @@ venue1 = Venue.create!(venue_name: "Bar Stock Exchange",
 )
 
 venue2 = Venue.create!(venue_name: "The White Swan",
-                       email: "thewhiteswan@mail.com", 
-                       password: "123456",
+                    #    email: "thewhiteswan@mail.com", 
+                    #    password: "123456",
                        address: "Pam Street, E1", 
                        phone_number: "", 
                        description: "Your next party destination",
@@ -45,8 +45,8 @@ venue2 = Venue.create!(venue_name: "The White Swan",
 )
 
 venue3 = Venue.create!(venue_name: "Plough and Harrow",
-                       email: "plough@mail.com", 
-                       password: "123456",
+                    #    email: "plough@mail.com", 
+                    #    password: "123456",
                        address: "12 Next Avenue W6 0PF", 
                        phone_number: "+443526970", 
                        description: "Your friendly neighbourhood pub",
@@ -54,8 +54,8 @@ venue3 = Venue.create!(venue_name: "Plough and Harrow",
 )
 
 venue4 = Venue.create!(venue_name: "Karma Club",
-                       email: "karmaclub@mail.com", 
-                       password: "123456",
+                    #    email: "karmaclub@mail.com", 
+                    #    password: "123456",
                        address: "14, East street, C1 987", 
                        phone_number: "+44462629383", 
                        description: "The best social",
@@ -63,8 +63,8 @@ venue4 = Venue.create!(venue_name: "Karma Club",
 )
 
 venue5 = Venue.create!(venue_name: "Bar Louis",
-                        email: "barlouis@mail.com", 
-                        password: "123456",
+                        # email: "barlouis@mail.com", 
+                        # password: "123456",
                         address: "Wiking street, Rose Avenue EC1 98", 
                         phone_number: "+44462629010", 
                         description: "The corner bar",
@@ -72,8 +72,8 @@ venue5 = Venue.create!(venue_name: "Bar Louis",
 )
 
 venue6 = Venue.create!(venue_name: "Tabula Rasa",
-                       email: "tabula@mail.com", 
-                       password: "123456",
+                    #    email: "tabula@mail.com", 
+                    #    password: "123456",
                        address: "23 Rant West , Avenue 2 Y3 987", 
                        phone_number: "+44462629383", 
                        description: "The best social",
@@ -83,8 +83,8 @@ venue6 = Venue.create!(venue_name: "Tabula Rasa",
 puts "creating 6 brands"
 #Brands
 brand1 = Brand.create!(brand_name: "Echo Falls",
-                       email: "brand1@mail.com",
-                       password: "123456",
+                    #    email: "brand1@mail.com",
+                    #    password: "123456",
                        address: "12, Lower East E1 H78",
                        phone_number: "+44236253798",
                        description: "We’re here to make your ordinary feel extraordinary. From our delicious Fruit Fusions, to smooth reds and refreshing whites, we've got a product for you.",
@@ -92,8 +92,8 @@ brand1 = Brand.create!(brand_name: "Echo Falls",
 )
 
 brand2 = Brand.create!(brand_name: "Bacardi",
-                       email: "brand2@mail.com",
-                       password: "123456",
+                    #    email: "brand2@mail.com",
+                    #    password: "123456",
                        address: "Albion Road North PT 876",
                        phone_number: "+44985342526",
                        description: "From a family dream in 1800’s Cuba, to celebrating the moments that matter today to our global community, this is the Bacardi story – over 150 years of bringing people together.",
@@ -101,8 +101,8 @@ brand2 = Brand.create!(brand_name: "Bacardi",
 )
 
 brand3 = Brand.create!(brand_name: "Jack Daniel's",
-                       email: "brand3@mail.com",
-                       password: "123456",
+                    #    email: "brand3@mail.com",
+                    #    password: "123456",
                        address: "Pullman Street TR 567",
                        phone_number: "+44986543009",
                        description: "You favourite whisky brand",
@@ -110,8 +110,8 @@ brand3 = Brand.create!(brand_name: "Jack Daniel's",
 )
 
 brand4 = Brand.create!(brand_name: "Gordon's",
-                       email: "brand4@mail.com",
-                       password: "123456",
+                    #    email: "brand4@mail.com",
+                    #    password: "123456",
                        address: "Windmill Street UI T65",
                        phone_number: "+4491222218",
                        description: "Gordon's high quality and distinctive juniper flavour has come to define the taste of the classic G&T. Gordon's today is a gin that's enjoyed, not dissected.",
@@ -119,8 +119,8 @@ brand4 = Brand.create!(brand_name: "Gordon's",
 )
 
 brand5 = Brand.create!(brand_name: "Peroni",
-                       email: "brand5@mail.com",
-                       password: "123456",
+                    #    email: "brand5@mail.com",
+                    #    password: "123456",
                        address: "La Mane Street YU *&^",
                        phone_number: "+4498",
                        description: "A STORY FROM ITALY In 1963, the Peroni family set out to create a beer that embodied Italian values of quality and craftsmanship. Since then, it’s been brewed with the same flair and attention to detail – delivering crisp, refreshing beers that are perfect for any occasion.",
@@ -128,8 +128,8 @@ brand5 = Brand.create!(brand_name: "Peroni",
 )
 
 brand6 = Brand.create!(brand_name: "Absolut Vodka",
-                       email: "brand6@mail.com",
-                       password: "123456",
+                    #    email: "brand6@mail.com",
+                    #    password: "123456",
                        address: "East avenue EC IR5",
                        phone_number: "+4494323558",
                        description: "Absolut Vodka is still a unique character, in both design and taste. Still ridiculously curious about creativity, collaborations and art. And still the key player for the perfect mix in any social occasion. And since all vodka from Absolut is produced in Southern Sweden, our superb vodka tasted exactly the same then as it does now. Nothing has changed.",
@@ -142,9 +142,9 @@ puts "Creating 6 Offers"
 offer1 = Offer.create!(venue_id: venue1.id, min_bid: "2500", contract_length: "24", estimated_volume: "1500 bottles", category: "Whisky")
 offer2 = Offer.create!(venue_id: venue2.id, min_bid: "3000", contract_length: "24", estimated_volume: "1400 bottles", category: "Gin")
 offer3 = Offer.create!(venue_id: venue3.id, min_bid: "2750", contract_length: "12", estimated_volume: "500 bottles", category: "Vodka")
-offer4 = Offer.create!(venue_id: venue4.id, min_bid: "1500", contract_length: "18", estimated_volume: "1000 kegs", category: "Beer")
-offer5 = Offer.create!(venue_id: venue5.id, min_bid: "4000", contract_length: "20", estimated_volume: "1200 bottles", category: "Wine")
-offer6 = Offer.create!(venue_id: venue6.id, min_bid: "3200", contract_length: "20", estimated_volume: "1200 bottles", category: "Rum")
+offer4 = Offer.create!(venue_id: venue4.id, min_bid: "1500", contract_length: "18", estimated_volume: "1000 bottles", category: "Vodka")
+offer5 = Offer.create!(venue_id: venue5.id, min_bid: "4000", contract_length: "20", estimated_volume: "1200 bottles", category: "Gin")
+offer6 = Offer.create!(venue_id: venue6.id, min_bid: "3200", contract_length: "20", estimated_volume: "1200 bottles", category: "Whisky")
 
 puts "Creating 6 bids"
 
