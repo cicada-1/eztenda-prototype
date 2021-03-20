1fe8aa24c27f41069fdbcfd26575d21d9b762a62
Peter Wallace <peterwallace@hotmail.co.uk>
2021-03-20 15:30:02 +0000

    Create basic form for offers

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,8 +2,10 @@ class Offer < ApplicationRecord
   belongs_to :venue
   has_many :bids
 
+  CATEGORIES = ["Gin", "Whiskey", "Whisky", "Vodka", "Rum", "Lager", "Bitter", "Cider", "Tonic Water", "Energy Drink"]
+
   validates :min_bid, precence: true
   validates :contract_length, precence: true
   validates :estimated_volume, precence: true
-
+  validates :category, inclusion: { in: CATEGORIES }
 end
