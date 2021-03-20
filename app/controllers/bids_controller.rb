298aabda4bfd2531ea8ab8960cdbc4dd9b48a882
gayatriarora <gayatri7arora@gmail.com>
2021-03-20 14:12:09 +0000

    Added reference to brand and added actions to brand and bid controller

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,16 +1,38 @@
 class BidsController < ApplicationController
   def index
+    @bid = Bid.all
   end
 
   def show
+    @bid = Bid.find(params[:id])
   end
 
   def new
+    @bid = Bid.new
   end
 
   def create
+    @bid = Bid.new(bid_params)
+    if @bid.save
+      redirect_to bid_path(@bid)
+    else
+      render :new 
+    end   
+  end
+
+    
+    
   end
 
   def destroy
+    @bid.delete(@bid)
   end
+
+private
+
+def bid_params
+  params[:bid].permit(:offer_amount, :additional)
+end
+
+
 end
