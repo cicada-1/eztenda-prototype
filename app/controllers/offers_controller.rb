3e483f89eed2f4d71266c8c4cea2e8c3fd384340
Peter Wallace <peterwallace@hotmail.co.uk>
2021-03-20 16:56:15 +0000

    Make sure all forms are working.

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,5 @@
 class OffersController < ApplicationController
-  before_action :set_venue, only: [:show, :edit, :destroy]
+  before_action :set_offer, only: [:show, :edit, :destroy]
 
   def index
     @offers = Offer.where(venue_id: current_user.venue.id)
@@ -10,13 +10,14 @@ class OffersController < ApplicationController
   end
 
   def new
+    @venue = Venue.find(params[:venue_id])
     @offer = Offer.new
   end
 
   def create
-    @offer = Offer.new(offer_params)
+    @offer = Offer.new(offer_params.merge(venue_id: params[:venue_id]))
     if @offer.save
-      redirect_to offer_path(@offer)
+      redirect_to venue_offers_path
     else
       render :new
     end
@@ -27,11 +28,11 @@ class OffersController < ApplicationController
 
   def update
     @offer.update(offer_params)
-    redirect_to offer_path(@offer)
+    redirect_to venue_offer_path(@offer)
   end
 
   def destroy
-    @venue.destroy
+    @offer.destroy
     redirect_to root_path
   end
 
@@ -42,6 +43,6 @@ class OffersController < ApplicationController
   end
 
   def offer_params
-    params.require(:offer).permit(:min_bid, :contract_length, :estimated_volume, :category)
+    params.require(:offer).permit(:min_bid, :contract_length, :estimated_volume, :category, :venue_id)
   end
 end
